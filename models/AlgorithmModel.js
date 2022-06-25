f66151f02360c21aa65a916cc9ff1ac5a60ac519
David Hatem <davidhatem880@gmail.com>
2022-06-25 20:25:24 +0200

    adding game progress

--- models/AlgorithmModel.js
+++ models/AlgorithmModel.js
@@ -5,11 +5,12 @@ const algorithm = new Schema(
   {
     title: String,
     intro: String,
+    game_progress: Boolean,
     topics: [{ type: mongoose.Schema.Types.ObjectId, ref: "Topic" }],
     quizes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Quiz" }],
     mainNotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "MainNotes" }],
     feedback: [{ type: mongoose.Schema.Types.ObjectId, ref: "Feedback" }],
-    createdby:{type: mongoose.Schema.Types.ObjectId, ref:"User"}
+    createdby: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
